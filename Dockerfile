8571c281734383324146aa0d4dcb98d7c24cc537
Mociofleaca Alin <alin.stefan.mociofleaca@gmail.com>
2023-06-08 11:57:12 +0300

    add first version

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3.9
+
+WORKDIR /app
+
+# Copy the source code into the container
+COPY src/app.py /app/app.py
+
+# Copy the requirements file into the container
+COPY requirements.txt /app/requirements.txt
+
+# Install the required dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Set the entrypoint to start the application
+ENTRYPOINT ["python", "app.py"]
---
+++ src/app.py
@@ -0,0 +1,52 @@
+from flask import Flask, request
+
+# We keep the data in a 'counter' file for peristency . We will create a volume 
+# that is mounted on the host. This ensure that the counter data is not lost even when we deploy a new version of the container.
+
+app = Flask(__name__)
+counter_file = '/data/counter.txt'
+
+
+def read_counter():
+    try:
+        with open(counter_file, 'r') as file:
+            return int(file.read())
+    except FileNotFoundError:
+        return 0
+
+
+def write_counter(counter):
+    with open(counter_file, 'w') as file:
+        file.write(str(counter))
+
+
+@app.route('/', methods=['GET'])
+def get_counter():
+    counter = read_counter()
+    return f'''
+    <html>
+        <head>
+            <title>Counter</title>
+        </head>
+        <body>
+            <h1>Counter: {counter}</h1>
+        </body>
+    </html>
+    '''
+
+
+@app.route('/', methods=['POST'])
+def increment_counter():
+    counter = read_counter()
+    counter += 1
+    write_counter(counter)
+    return 'POST request received'
+
+
+@app.route('/health', methods=['GET'])
+def health_check():
+    return 'OK'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
