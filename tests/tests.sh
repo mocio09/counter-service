13b2c35c029bca9820642291a7534e25df511aac
Mociofleaca Alin <alin.stefan.mociofleaca@gmail.com>
2023-06-08 19:50:11 +0300

    add tests

---
+++ tests/tests.sh
@@ -0,0 +1,20 @@
+response=$(curl -sS http://localhost:8212/health)
+if [ "$response" == "OK" ]; then
+    echo "Health check passed. The server is healthy."
+else
+    echo "Health check failed. The server is not healthy."
+fi
+
+response_post=$(curl -sS -o /dev/null -w "%{http_code}" -X POST http://localhost:8212/)
+if [ "$response" == "200" ]; then
+    echo "POST request succeeded. Status code: 200"
+else
+    echo "POST request failed. Status code: $response"
+fi
+
+response_post=$(curl -sS -o /dev/null -w "%{http_code}" -X GET http://localhost:8212/)
+if [ "$response" == "200" ]; then
+    echo "POST request succeeded. Status code: 200"
+else
+    echo "POST request failed. Status code: $response"
+fi
